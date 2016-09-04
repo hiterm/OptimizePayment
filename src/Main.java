ad469f4d10b5462e0a28a5b55bbd52dcf2331630
htlsne <htlsne@users.noreply.github.com>
2016-09-04 23:44:25 +0900

    Format and update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,12 @@
-import java.util.function.IntConsumer;
 import java.util.function.IntSupplier;
 
 public class Main {
-	public static void main(String[] args) {
-		MoneySet set = new MoneySet();
-		
-		IntConsumer consumer = set::setB5000;
-		consumer.accept(3);
-		
-		IntSupplier func;
-		func = set::getAmount;
-		System.out.println(func.getAsInt());
-	}
+    public static void main(String[] args) {
+        MoneySet set;
+        set = MoneySet.valueOf(13720);
+
+        IntSupplier func;
+        func = set::getAmount;
+        System.out.println(func.getAsInt());
+    }
 }
